c67a2b4ef7ed241c185a6838b6134ffd602e4fe2
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-17 12:45:24 +0300

    feat: complete finalTask module4.2 v.0

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,10 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 0; i <= cathetusLength - 1; i++) {
+            for (int j = 1; j <= cathetusLength; j++){
+                if (j >= cathetusLength - i) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
+
         new HalfPyramid().printHalfPyramid(8);
     }
 }
